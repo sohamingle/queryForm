40a66319b93fc6663c4430c2bcdeb8fcfb9842da
sohamingle <sohamingle@gmail.com>
2023-09-17 20:32:05 +0530

    style

--- app/Form.jsx
+++ app/Form.jsx
@@ -50,7 +50,7 @@ const QueryForm = () => {
   return (
     <main className="h-screen flex flex-col justify-center items-center md:p-3">
         <ToastContainer/>
-      <form onSubmit={handleSubmit} className="flex flex-col w-screen h-screen lg:max-w-lg md:max-w-md:flex sm:max-w-sm bg-white p-7 rounded-2xl">
+      <form onSubmit={handleSubmit} className="flex flex-col w-screen h-screen max-h-lg lg:max-w-lg md:max-w-md:flex sm:max-w-sm bg-white p-7 rounded-2xl">
         <div className="flex flex-row h-16 items-center font-arial text-xl text-black w-full">
             <Image src={doc} alt="doc.png" width={20} className="object-contain mx-2 h-auto"/>
             <div>
