25200cb103724b764b9a03f923daba7fbd544c8b
sohamingle <139986894+sohamingle@users.noreply.github.com>
2023-09-18 16:46:51 +0530

    fixes

--- app/api/getQueries/route.js
+++ app/api/getQueries/route.js
@@ -2,12 +2,8 @@ import prisma from "@/prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  try {
-      const queries = await prisma.query.findMany();
-          return new NextResponse.json(queries);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-                    return new NextResponse.error(error.message, { status: 500 });
-                      }
-                      }
+
+    const queries = await prisma.query.findMany();
+    return new NextResponse.json(queries);
+    }
                       
