bbff9f7fb53226c8d47c4b162542c5afb8906d11
sohamingle <sohamingle@gmail.com>
2023-09-20 17:04:16 +0530

    logo

--- app/page.js
+++ app/page.js
@@ -4,8 +4,13 @@ import logo from '../public/logo.png'
 
 export default function Home() {
   return (
-      <main>
-        <QueryForm />
+      <main className='flex justify-between'>
+        <div className='bg-white flex items-center p-20'>
+          <Image src={logo} alt="logo" width={500} height={100} />
+        </div>
+        <div className='flex justify-center mr-28'>
+          <QueryForm />
+        </div>
       </main>
   )
 }
