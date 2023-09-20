3dfcc6bf849d67778f5aaf863a64a1572aa5365a
sohamingle <sohamingle@gmail.com>
2023-09-20 16:16:07 +0530

    logo

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import QueryForm from './Form'
+import logo from '../public/logo.png'
 
 export default function Home() {
   return (
-      <main>
+      <main className='flex justify-between'>
+        <div className='bg-white left-0 flex justify-center items-center p-10'>
+          <Image src={logo} alt='logo' width={500}/>
+        </div>
         <QueryForm />
       </main>
   )
