5dc05d95c836926a8e543a89434c8792e480cab3
sohamingle <sohamingle@gmail.com>
2023-09-24 17:07:50 +0530

    auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,15 @@
 import NextAuth from "next-auth/next"
 import CredentialsProvider from "next-auth/providers/credentials"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth(
     {
 
         providers: [
+          GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+          }),
           CredentialsProvider({
             name: 'Credentials',
             credentials: {
