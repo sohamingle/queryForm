b2dc7c1c344b058322eb45d1c0aded0232ffd098
sohamingle <sohamingle@gmail.com>
2023-10-03 10:02:41 +0530

    auth

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,9 +1,10 @@
 import QueryTable from "./QueryTable";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const AdminPage= async() => {
-    const session = await getServerSession()
+    const session = await getServerSession(authOptions)
   if(!session || !session.user){
     redirect('/auth/signIn')
   }
